66638cfd0dc9d6de3208a161a4763008c051a5c4
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-03-11 00:08:20 +0800

    Complete basic functions
    This program can generate random string by srand() and rand(). However, it's not cryptographically secure. I will use secure method from Libsodium to substitute them.

---
+++ RandomStringGenerator/RandomStringGenerator/RandomStringGenerator.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+
+	char charSet[] = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int cur = 0, length = 0, num = 0, mod = strlen(charSet);
+	char curChar = '1';
+
+	printf("Input the length of random string(s): \n");
+	scanf_s("%d", &length);
+	printf("Input the number of random string(s) you need: \n");
+	scanf_s("%d", &num);
+
+	printf("===================Result====================\n");
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{
+			cur = rand();
+			curChar = charSet[cur % mod];
+			printf("%c", curChar);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
