55c79d849323bf8b1c87478ad9b2a5b37cb1b6ee
HBSnail <47233303+HBSnail@users.noreply.github.com>
2023-03-15 20:36:05 +0800

    Load dynamic library by windows API  to trim project files.

--- RandomStringGenerator/RandomStringGenerator.cpp
+++ RandomStringGenerator/RandomStringGenerator.cpp
@@ -2,49 +2,61 @@
 #include <ctime>
 #include <cstdlib>
 #include <cstring>
-#include "sodium.h"
+#include <Windows.h>
 
-using namespace std;
+typedef int(__cdecl* SODIUM_INIT)();
+typedef unsigned int(__cdecl* RANDOMBYTES_UNIFORM)(unsigned int CharSetCard);
 
 int main()
 {
-	if (sodium_init() < 0)
-	{
-		printf("Libsodium is not correctly initialized. The program is going to exit.");
-		
+	HINSTANCE hinstLib = LoadLibrary(L"libsodium.dll");
+	
+	if (hinstLib == NULL) {
+		printf("Libsodium is not correctly loaded. The program is going to exit.");
+
 		system("pause");
 
 		return 1;
 	}
-	else
+	SODIUM_INIT sodium_init = (SODIUM_INIT)GetProcAddress(hinstLib, "sodium_init");
+	RANDOMBYTES_UNIFORM randombytes_uniform = (RANDOMBYTES_UNIFORM)GetProcAddress(hinstLib, "randombytes_uniform");
+
+	if (sodium_init() < 0)
 	{
-		char charSet[] = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		uint32_t cur = 0, charSetCard = strlen(charSet);
-		int length = 0, num = 0;
-		char curChar = '1';
-
-		printf("Input the length of random string(s): \n");
-		scanf_s("%d", &length);
-		printf("Input the number of random string(s) you need: \n");
-		scanf_s("%d", &num);
-
-		printf("===================Result====================\n");
-
-		for (int i = 0; i < num; i++)
-		{
-			for (int j = 0; j < length; j++)
-			{
-				cur = randombytes_uniform(charSetCard);
-				curChar = charSet[cur];
-				printf("%c", curChar);
-			}
-			printf("\n");
-		}
-
-		printf("============================================\n");
+		printf("Libsodium is not correctly initialized. The program is going to exit.");
 
 		system("pause");
 
-		return 0;
+		return 1;
 	}
+
+	char charSet[] = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	unsigned int cur = 0, charSetCard = strlen(charSet);
+	int length = 0, num = 0;
+	char curChar = '1';
+
+	printf("Input the length of random string(s): \n");
+	scanf_s("%d", &length);
+	printf("Input the number of random string(s) you need: \n");
+	scanf_s("%d", &num);
+
+	printf("===================Result====================\n");
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{
+			cur = randombytes_uniform(charSetCard);
+			curChar = charSet[cur];
+			printf("%c", curChar);
+		}
+		printf("\n");
+	}
+
+	printf("============================================\n");
+
+	system("pause");
+
+	return 0;
+
 }
